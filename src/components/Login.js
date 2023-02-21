caf1e367e9ad6093ea29a393cff953c5753dc534
annabel <annatae@DESKTOP-N62GGGT.localdomain>
2023-02-21 20:56:14 +0800

    redux tutorial boilerplate

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Log in here</h1>
+      <button>Log in here</button>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <h1> Profile Page </h1>
+
+      <h2> Username: </h2>
+      <h3> Your Age: </h3>
+      <h3> Your email: </h3>
+    </>
+  );
+};
+
+export default Profile;
